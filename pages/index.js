2877ee05eb5b1bd0d96bc5a33c6f22dc776fbc77
ohunyanC <wildcardcharly@outlook.com>
2022-10-22 11:15:14 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,12 @@ export default function Home({ posts }) {
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
-            <PostCard post={post.node} key={post.title} />
+          {posts.map((post, index) => (
+            <PostCard post={post.node} key={index} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
